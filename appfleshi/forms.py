c7f88feeecb83bb3d2d735cc9c6649e771b7ec0f
Kauan Dias <kauan.dias.santos03@gmail.com>
2025-12-01 16:53:03 -0300

    feat: imagem banco

---
+++ appfleshi/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, FileField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
+from appfleshi.models import User
+
+class PhotoForm(FlaskForm):
+    photo = FileField("Foto",validators=[DataRequired()])
+    submit = SubmitField("Postar")
+
+class LoginForm(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    password = PasswordField("Senha", validators=[DataRequired()])
+    submit = SubmitField("Entrar")
+
+class RegisterForm(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    username = StringField("Nome de usuário", validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField("Senha", validators=[DataRequired(), Length(min=6, max=60)])
+    confirm_password = PasswordField("Confirmar senha", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Criar conta")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            return ValidationError("Email já cadastrado!")
+        return None
