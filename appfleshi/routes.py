46aa48d4e0593910ce018ea2c8007ea438c26e02
Kauan Dias <kauan.dias.santos03@gmail.com>
2025-12-09 13:34:21 -0300

    Add files via upload

--- appfleshi/routes.py
+++ appfleshi/routes.py
@@ -13,7 +13,7 @@ def homepage():
         user = User.query.filter_by(email=login_form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, login_form.password.data):
             login_user(user)
-            return redirect(url_for('profile', user_id=user.id))
+            return redirect(url_for('feed', order="desc"))
     return render_template('homepage.html', form=login_form)
 
 @app.route('/profile/<user_id>', methods=['GET', 'POST'])
@@ -52,3 +52,13 @@ def createaccount():
 def logout():
     logout_user()
     return redirect(url_for('homepage'))
+
+@app.route("/feed/<order>")
+@login_required
+def feed(order):
+
+    if order == "desc":
+        photos = Photo.query.order_by(Photo.upload_date.desc()).all()
+    else:
+        photos = Photo.query.order_by(Photo.upload_date.asc()).all()
+    return render_template("feed.html", photos=photos)
